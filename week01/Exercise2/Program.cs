f6273702e36874c4525d12f82483fb4a4e378b3e
clane83 <clane83@yahoo.com>
2025-03-03 21:08:42 -0800

    week1 exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -6,28 +6,52 @@ class Program
     {
         Console.WriteLine("What is your grade percentage?(ex. 90) ");
         int grade = int.Parse(Console.ReadLine());
+        int last_digit = grade % 10;
+        string letter_grade = "";
+        string sign = "";
+        string a_or_an = "a";
+
+
+
+        if (last_digit >= 7)
+        {
+            sign = "+";
+        }
+        else if (last_digit < 3)
+        {
+            sign = "-";
+        }
         
+
+
+
         if (grade >= 90)
         {
-            Console.WriteLine("A");
+            letter_grade = "A";
+            if (grade >97) {sign = "";}
         }
         else if (grade >= 80)
         {
-            Console.WriteLine("B");
+            letter_grade = "B";
         }
         else if (grade >= 70)
         {
-            Console.WriteLine("C");
+            letter_grade = "C";
         }
         else if (grade >= 60)
         {
-            Console.WriteLine("D");
+            letter_grade = "D";
         }
         else
         {
-            Console.WriteLine("F");
+            letter_grade = "F";
+            sign = "";
         }
 
+        if (letter_grade == "A")
+        { a_or_an = "an";}
+
+        Console.WriteLine($"Your grade is {a_or_an} {letter_grade}{sign}");
 
     }
 }
