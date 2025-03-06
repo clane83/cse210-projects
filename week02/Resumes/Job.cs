c1b5d2913807f63b632194931241830ec27732c8
clane83 <clane83@yahoo.com>
2025-03-05 20:21:13 -0800

    updated job.cs and added 2 jobs to program.cs

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -8,9 +8,10 @@ public class Job
         public int _startYear;
         public int _endYear;
 
-    public override string ToString()
+    public string Display()
     {
-        return _jobTitle;
+        return $"{_jobTitle} ({_company}) {_startYear}-{_endYear}";
+        
     }
     
 
