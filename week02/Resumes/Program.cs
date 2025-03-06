e7fe78da887f025b857e4995e48805bbacf2de64
clane83 <clane83@yahoo.com>
2025-03-05 20:08:55 -0800

    created job.cs and resume.cs print job1 to screen

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineering";
+
+        Console.WriteLine(job1);
+
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Resumes
+{
+    static void Display(string[] args)
+    {
+    
+        string _name;
+        List<string> _jobs = new List<string>();
+
+    }
+}
