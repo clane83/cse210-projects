7c608d71ee5c75078165121d2e8e9fcd92501cd6
clane83 <clane83@yahoo.com>
2025-03-06 19:28:59 -0800

    Updated Resume and program.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,10 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
+        Resumes resume1 = new Resumes();
         job1._jobTitle = "Software Engineering";
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
+        
         
         Job job2 = new Job();
         job2._jobTitle = "Manager";
@@ -16,9 +18,13 @@ class Program
         job2._startYear = 2022;
         job2._endYear = 2023;
 
+        resume1._name = "Tina Lane";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
 
-        Console.WriteLine(job1.Display());
-        Console.WriteLine(job2.Display());
+        Console.WriteLine(resume1.Display());
+        // Console.WriteLine(job1.Display());
+        // Console.WriteLine(job2.Display());
        
 
     }
--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 public class Resumes
 {
-    static void Display(string[] args)
-    {
-    
-        string _name;
-        List<string> _jobs = new List<string>();
+    public string _name;
+    public List<Job> _jobs = new List<Job>(); // Change List<string> to List<Job>
 
+    public string Display()
+    {
+        string result = $"Name: {_name}\nJobs:\n";
+
+        // Loop through each job in the list and display details
+        foreach (Job job in _jobs)
+        {
+            result += $"- {job.Display()}\n";
+        }
+
+        return result;
     }
 }
