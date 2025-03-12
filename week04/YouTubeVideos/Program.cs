3c8141acc267ada9ad389e70f31caeb2c23fc701
clane83 <clane83@yahoo.com>
2025-03-12 14:11:25 -0700

    added videos to week 4

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -12,7 +13,21 @@ class Program
         video1.AddComments("Jamie", "Wonderful video");
         video1.AddComments("Victoria", "How did you do that?");
 
+        Video video2 = new Video{_author = "Bailey", _title = "Makeup Tutorial", _length = "120"};
+
+        video2.AddComments("Tina", "That looks adorable");
+        video2.AddComments("Jamie", "Great job");
+        video2.AddComments("Victoria", "OMG, love it");
+
+        Video video3 = new Video{_author = "Bailey", _title = "Dance Tutoria", _length = "120"};
+
+        video3.AddComments("Tina", "You are really good at dancing");
+        video3.AddComments("Jamie", "The splits at the end, loved it");
+        video3.AddComments("Victoria", "Wow");
+
         videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
 
         foreach (var video in videos)
         {
