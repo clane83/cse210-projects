5df1bab5cf87e029462c346dd54233706e7c2aa8
clane83 <clane83@yahoo.com>
2025-04-11 19:55:46 -0700

    start of week7

---
+++ week07/ExerciseTracking/Exercise.cs
@@ -0,0 +1,12 @@
+// Distance (km) = swimming laps * 50 / 1000
+// Distance (miles) = swimming laps * 50 / 1000 * 0.62
+// Speed (mph or kph) = (distance / minutes) * 60
+// Pace (min per mile or min per km)= minutes / distance
+// Speed = 60 / pace
+// Pace = 60 / speed
+
+// In addition, your program must:
+
+// Use inheritance to avoid duplicating shared attributes and methods.
+// Use method overriding for the calculation methods.
+// Follow the principles of encapsulation, making sure each member variable is private.
---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,1 @@
+//Swimming: number of laps
